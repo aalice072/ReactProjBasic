c290c1be0374891922c9df8b713fc5f17af64998
Vasudev <tech3in@live.com>
2019-05-11 15:35:06 +0530

    btn addon

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, Button, StyleSheet} from 'react-native'
 
 const mystyles = StyleSheet.create({
   myblue:{
@@ -7,24 +7,47 @@ const mystyles = StyleSheet.create({
   }
 })
 
+
+
 export class MyComp extends Component{
 
   constructor(props){
     super(props)
+    this.state = {myValue: 1};
+
+    const timer = setInterval(()=>{
+      this.setState(oldObj=>(
+        {myValue:oldObj.myValue+1}
+      ))
+    },1000);
+   // clearInterval(t)
   }
+
+  
 
   render(){
     return <View>
-      <Text style={mystyles.myblue}>{this.props.myname}</Text>
+      <Text style={mystyles.myblue}>{this.props.myname} : {this.state.myValue}</Text>
     </View>
   }
 }
 
 export default class HelloWorld extends Component{
   
+  constructor(props){
+    super(props)
+    this.state = {text: "Button Default Title"}
+  }
+ 
+
+  onMyBtnPressed = () => this.setState({text:"Value changed"})
 
   render(){
    // return <Text style={mystyles.myblue}>Hello Wolrd</Text>
-   return <MyComp myname="Hello World"></MyComp>
+   return <View>
+     <MyComp myname="Hello World"></MyComp>
+     <Button title={this.state.text} color="#841584" onPress={this.onMyBtnPressed}></Button>
+   </View>
+   
   }
 }
